400258b6b40307b0942ac53dc8a94d5582f7c826
Phoenix-neat <71899553+Phoenix-neat@users.noreply.github.com>
2021-01-25 10:57:45 -0500

    Add files via upload

---
+++ Party_Skill_Editor_P3.py
@@ -0,0 +1,98 @@
+import skillID
+import partySkills
+
+#checks if for vanilla FES or New Moon
+newMoonCheck = input("Type 'F' for unmodded Persona 3 FES, or 'M' for Persona 3 FES with the New Moon Balance Patch: ")
+if newMoonCheck not in ["F", "f", "M", "m"]:
+    errorMessage = input("Invalid response. Only 'F' and 'M' are acceptable")
+    raise SystemExit
+
+#checks if for The Journey or The Answer
+gameCheck = input("Type 'J' for The Journey, or 'A' for The Answer: ")
+if gameCheck not in ["J", "j", "A", "a"]:
+    errorMessage = input("Invalid response. Only 'J' and 'A' are acceptable")
+    raise SystemExit
+
+#checks the level for each party member
+print("Enter in the levels of each party member. If you do not have the party member, enter '0'")
+yukariLvl = int(input("Yukari: "))
+junpeiLvl = int(input("Junpei: "))
+akihikoLvl = int(input("Akihiko: "))
+mitsuruLvl = int(input("Mitsuru: "))
+aigisLvl = int(input("Aigis: "))
+koromaruLvl = int(input("Koromaru: "))
+kenLvl = int(input("Ken: "))
+if gameCheck in  ["J", "j"]:
+    shinjiroLvl = int(input("Shinjiro: "))
+elif gameCheck in ["A", "a"]:
+    metisLvl = int(input("Metis: "))
+
+overwrite = open("test.txt", "w")
+overwrite.write("")
+pnach = open("test.txt", "a")
+
+#for Yukari (All, includes New Moon J/A and Vanilla J/A)
+if yukariLvl != 0:
+    if newMoonCheck in ["M", "m"]:
+        partySkills.yukSkills.remove(skillID.Amrita)
+        partySkills.yukSkills.remove(skillID.Panta_Rhei)
+    if newMoonCheck in ["F", "f"]:
+        if yukariLvl >= 55:
+            partySkills.yukSkills.remove(skillID.Me_Patra)
+        if yukariLvl >= 80:
+            partySkills.yukSkills.remove(skillID.Garudyne)
+    if yukariLvl >= 25:
+        partySkills.yukSkills.remove(skillID.Dia)
+    if yukariLvl not in range(4, 35):
+        partySkills.yukSkills.remove(skillID.Patra)
+    if yukariLvl not in range(5, 31):
+        partySkills.yukSkills.remove(skillID.Garu)
+    if yukariLvl < 16:
+        partySkills.yukSkills.remove(skillID.Charmdi)
+    if yukariLvl not in range(21, 45):
+        partySkills.yukSkills.remove(skillID.Magaru)
+    if yukariLvl not in range(22, 42):
+        partySkills.yukSkills.remove(skillID.Media)
+    if yukariLvl not in range(25, 51):
+        partySkills.yukSkills.remove(skillID.Diarama)
+    if yukariLvl not in range(28, 67):
+        partySkills.yukSkills.remove(skillID.Recarm)
+    if yukariLvl not in range(32, 57):
+        partySkills.yukSkills.remove(skillID.Garula)
+    if yukariLvl < 36:
+        partySkills.yukSkills.remove(skillID.Me_Patra)
+    if yukariLvl not in range(43, 74):
+        partySkills.yukSkills.remove(skillID.Mediarama)
+    if yukariLvl not in range(46, 65):
+        partySkills.yukSkills.remove(skillID.Magarula)
+    if yukariLvl < 52:
+        partySkills.yukSkills.remove(skillID.Diarahan)
+    if yukariLvl < 57:
+        partySkills.yukSkills.remove(skillID.Garudyne)
+    if yukariLvl < 60:
+        partySkills.yukSkills.remove(skillID.Wind_Break)
+    if yukariLvl < 65:
+        partySkills.yukSkills.remove(skillID.Magarudyne)
+    if yukariLvl < 68:
+        partySkills.yukSkills.remove(skillID.Samarecarm)
+    if yukariLvl < 74:
+        partySkills.yukSkills.remove(skillID.Mediarahan)
+
+    pnach.write(
+        "//Yukari skills \n//Yukari Persona Skill 1\npatch=1,EE,108340F8,extended,00000" + partySkills.yukSkills[0])
+    if len(partySkills.yukSkills) > 1:
+        pnach.write("\n//Yukari Persona Skill 2\npatch=1,EE,108340FA,extended,00000" + partySkills.yukSkills[1])
+    if len(partySkills.yukSkills) > 2:
+        pnach.write("\n//Yukari Persona Skill 3\npatch=1,EE,108340FC,extended,00000" + partySkills.yukSkills[2])
+    if len(partySkills.yukSkills) > 3:
+        pnach.write("\n//Yukari Persona Skill 4\npatch=1,EE,108340FE,extended,00000" + partySkills.yukSkills[3])
+    if len(partySkills.yukSkills) > 4:
+        pnach.write("\n//Yukari Persona Skill 5\npatch=1,EE,10834100,extended,00000" + partySkills.yukSkills[4])
+    if len(partySkills.yukSkills) > 5:
+        pnach.write("\n//Yukari Persona Skill 6\npatch=1,EE,10834102,extended,00000" + partySkills.yukSkills[5])
+    if len(partySkills.yukSkills) > 6:
+        pnach.write("\n//Yukari Persona Skill 7\npatch=1,EE,10834104,extended,00000" + partySkills.yukSkills[6])
+    if len(partySkills.yukSkills) > 7:
+        pnach.write("\n//Yukari Persona Skill 8\npatch=1,EE,10834106,extended,00000" + partySkills.yukSkills[7])
+
+#for Junpei (vanilla Journey)
---
+++ partySkills.py
@@ -0,0 +1,20 @@
+import skillID
+
+yukSkills = [skillID.Dia, skillID.Patra, skillID.Garu, skillID.Charmdi, skillID.Magaru, skillID.Media, skillID.Diarama,
+             skillID.Recarm, skillID.Garula, skillID.Me_Patra, skillID.Mediarama, skillID.Magarula, skillID.Diarahan,
+             skillID.Amrita, skillID.Garudyne, skillID.Wind_Break, skillID.Magarudyne, skillID.Samarecarm,
+             skillID.Mediarahan, skillID.Panta_Rhei]
+
+junSkills = []
+
+akiSkills = []
+
+mitSkills = []
+
+aigSkills = []
+
+koroSkills = []
+
+kenSkills = []
+
+shinSkills = []
