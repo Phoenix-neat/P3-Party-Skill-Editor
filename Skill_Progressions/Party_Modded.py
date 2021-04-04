b7234df7239e9f7de1f1775fb8d1686f7b631ff7
Phoenix-neat <71899553+Phoenix-neat@users.noreply.github.com>
2021-04-03 20:37:25 -0400

    Add files via upload

---
+++ Skill_Progressions/Party_Modded.py
@@ -0,0 +1,270 @@
+from skillID import *
+
+yukari = [
+        (1, 25, Dia),
+        (4, 36, Patra),
+        (5, 32, Garu),
+        (16, 100, Charmdi),
+        (21, 46, Magaru),
+        (22, 43, Media),
+        (25, 100, Diarama),
+        (28, 100, Recarm),
+        (32, 100, Garula),
+        (36, 100, Me_Patra),
+        (43, 100, Mediarama),
+        (46, 100, Magarula)
+]
+
+yukariEv = [
+        (1, 25, Dia),
+        (4, 36, Patra),
+        (5, 32, Garu),
+        (16, 100, Charmdi),
+        (21, 46, Magaru),
+        (22, 43, Media),
+        (25, 52, Diarama),
+        (28, 68, Recarm),
+        (32, 57, Garula),
+        (36, 55, Me_Patra),
+        (43, 74, Mediarama),
+        (46, 65, Magarula),
+        (52, 100, Diarahan),
+        (55, 100, Amrita),
+        (57, 80, Garudyne),
+        (60, 100, Wind_Break),
+        (65, 100, Magarudyne),
+        (68, 100, Samarecarm),
+        (74, 100, Mediarahan),
+        (80, 100, Panta_Rhei)
+]
+
+junpei = [
+        (1, 18, Cleave),
+        (5, 30, Agi),
+        (7, 50, Re_Patra),
+        (9, 34, Rakukaja),
+        (20, 40, Double_Fangs),
+        (25, 55, Kill_Rush),
+        (30, 100, Agilao),
+        (31, 45, Counter),
+        (34, 100, Marakukaja),
+        (40, 100, Torrent_Shot),
+        (45, 100, Counterstrike),
+        (50, 100, Blade_of_Fury),
+        (55, 100, Gigantic_Fist)
+]
+
+junpeiEv = [
+        (1, 100, Spring_of_Life),
+        (1, 18, Cleave),
+        (5, 30, Agi),
+        (7, 50, Re_Patra),
+        (9, 34, Rakukaja),
+        (20, 40, Double_Fangs),
+        (25, 55, Kill_Rush),
+        (30, 64, Agilao),
+        (31, 45, Counter),
+        (34, 100, Marakukaja),
+        (40, 75, Torrent_Shot),
+        (45, 58, Counterstrike),
+        (50, 65, Blade_of_Fury),
+        (55, 80, Gigantic_Fist),
+        (58, 100, High_Counter),
+        (60, 100, Power_Charge),
+        (64, 100, Agidyne),
+        (65, 100, Brave_Blade),
+        (75, 100, Vorpal_Blade),
+        (80, 100, Phys_Skill_Up)
+]
+
+akihiko = [
+        (1, 100, Sonic_Punch),
+        (1, 29, Zio),
+        (1, 38, Dia),
+        (16, 100, Tarunda),
+        (21, 41, Mazio),
+        (25, 100, Rakunda),
+        (29, 100, Zionga),
+        (33, 100, Sukunda),
+        (37, 100, Elec_Boost),
+        (38, 100, Diarama),
+        (41, 100, Mazionga)
+]
+
+akihikoEv = [
+        (1, 50, Sonic_Punch),
+        (1, 29, Zio),
+        (1, 38, Dia),
+        (16, 47, Tarunda),
+        (21, 41, Mazio),
+        (25, 57, Rakunda),
+        (29, 53, Zionga),
+        (33, 66, Sukunda),
+        (37, 76, Elec_Boost),
+        (38, 65, Diarama),
+        (41, 74, Mazionga),
+        (47, 100, Matarunda),
+        (50, 100, Fist_Master),
+        (53, 80, Ziodyne),
+        (57, 100, Marakunda),
+        (65, 100, Diarahan),
+        (66, 100, Debilitate),
+        (74, 100, Maziodyne),
+        (76, 100, Elec_Amp),
+        (80, 100, Thunder_Reign)
+]
+
+mitsuru = [
+        (1, 21, Bufu),
+        (1, 42, Mabufu),
+        (1, 25, Dia),
+        (1, 50, Marin_Karin),
+        (21, 55, Bufula),
+        (25, 100, Diarama),
+        (27, 100, Spirit_Drain),
+        (32, 100, Tentarafoo),
+        (42, 100, Mabufula),
+        (45, 100, Ice_Boost),
+        (50, 100, Mind_Charge),
+        (55, 100, Bufudyne)
+]
+
+mitsuruEv = [
+        (1, 21, Bufu),
+        (1, 42, Mabufu),
+        (1, 25, Dia),
+        (1, 50, Marin_Karin),
+        (21, 55, Bufula),
+        (25, 58, Diarama),
+        (27, 100, Spirit_Drain),
+        (32, 100, Tentarafoo),
+        (42, 71, Mabufula),
+        (45, 76, Ice_Boost),
+        (50, 100, Mind_Charge),
+        (55, 80, Bufudyne),
+        (58, 100, Diarahan),
+        (61, 100, Ice_Break),
+        (71, 100, Mabufudyne),
+        (76, 100, Ice_Amp),
+        (80, 100, Niflheim)
+]
+
+aigis = [
+        (1, 35, Kill_Rush),
+        (1, 51, Swift_Strike),
+        (1, 47, Sukukaja),
+        (32, 56, Rakukaja),
+        (35, 73, Fatal_End),
+        (36, 60, Tarukaja),
+        (42, 100, Dekunda),
+        (47, 100, Heat_Riser),
+        (51, 77, Heat_Wave),
+        (56, 100, Marakukaja),
+        (59, 100, Diarahan),
+        (60, 100, Matarukaja)
+]
+
+aigisEv = [
+        (1, 35, Kill_Rush),
+        (1, 51, Swift_Strike),
+        (1, 47, Sukukaja),
+        (32, 56, Rakukaja),
+        (35, 73, Fatal_End),
+        (36, 60, Tarukaja),
+        (42, 100, Dekunda),
+        (47, 100, Masukukaja),
+        (51, 77, Heat_Wave),
+        (56, 100, Marakukaja),
+        (59, 100, Diarahan),
+        (60, 100, Matarukaja),
+        (65, 100, Samarecarm),
+        (73, 100, Akasha_Arts),
+        (77, 100, Gods_Hand)    
+]
+
+koromaru = [
+        (1, 45, Agilao),
+        (1, 33, Mudo),
+        (1, 50, Sukukaja),
+        (1, 100, Apt_Pupil),
+        (33, 71, Mudoon),
+        (34, 67, Maragion),
+        (39, 77, Fire_Boost),
+        (40, 75, Mighty_Swing),
+        (42, 49, Mamudo),
+        (45, 80, Agidyne),
+        (49, 100, Mamudoon),
+        (50, 100, Masukukaja),
+        (52, 100, High_Counter),
+        (60, 100, Mudo_Boost),
+        (67, 100, Maragidyne),
+        (71, 100, Desecrate),
+        (75, 100, Heavens_Blade),
+        (77, 100, Fire_Amp),
+        (80, 100, Ragnarok)
+]
+
+ken = [
+        (1, 35, Hama),
+        (1, 100, Cruel_Attack),
+        (1, 100, Zionga),
+        (1, 100, Sukunda),
+        (1, 100, Diarama),
+        (32, 100, Recarm),
+        (35, 100, Hamaon),
+        (37, 100, Mediarama)
+]
+
+kenEv = [
+        (1, 35, Hama),
+        (1, 46, Cruel_Attack),
+        (1, 55, Zionga),
+        (1, 42, Sukunda),
+        (1, 60, Diarama),
+        (32, 71, Recarm),
+        (35, 74, Hamaon),
+        (37, 76, Mediarama),
+        (42, 100, Masukunda),
+        (46, 65, Vile_Assault),
+        (54, 100, Spear_Master),
+        (55, 80, Ziodyne),
+        (60, 100, Diarahan),
+        (65, 100, Primal_Force),
+        (71, 100, Samarecarm),
+        (74, 100, Sanctify),
+        (76, 100, Mediarahan),
+        (80, 100, Thunder_Reign)
+]
+
+shinjiro = [
+        (1, 65, Regenerate1),
+        (1, 42, Counter),
+        (1, 77, Fatal_End),
+        (39, 100, Evil_Smile),
+        (42, 55, Counterstrike),
+        (50, 100, Power_Charge),
+        (52, 60, Blade_of_Fury),
+        (53, 72, Heat_Wave),
+        (55, 100, High_Counter),
+        (60, 100, Deathbound),
+        (65, 75, Regenerate2),
+        (72, 100, Akasha_Arts),
+        (75, 100, Regenerate3),
+        (77, 100, Gods_Hand),
+        (80, 100, Phys_Skill_Up)
+]
+
+#Determines if Personas are evolved
+monthCheck = int(input("Enter the month number (Example: December is 12): "))
+dayCheck = int(input("Enter the day number (No example you can do this): "))
+if monthCheck > 10 or monthCheck == 10 and dayCheck > 4:
+        akihiko = akihikoEv
+        ken = kenEv
+if monthCheck > 11 or monthCheck == 11 and dayCheck > 7:
+        yukari = yukariEv
+if monthCheck > 11 or monthCheck == 11 and dayCheck > 17:
+        mitsuru = mitsuruEv
+if monthCheck > 11 or monthCheck == 11 and dayCheck > 22:
+        junpei = junpeiEv
+if monthCheck > 12 or monthCheck == 12 and dayCheck > 30:
+        aigis = aigisEv
---
+++ Skill_Progressions/Party_Modded_A.py
@@ -0,0 +1,129 @@
+from skillID import *
+
+yukari = [
+        (1, 36, Patra),
+        (1, 32, Garu),
+        (1, 100, Charmdi),
+        (1, 46, Magaru),
+        (1, 43, Media),
+        (1, 52, Diarama),
+        (28, 68, Recarm),
+        (32, 57, Garula),
+        (36, 55, Me_Patra),
+        (43, 74, Mediarama),
+        (46, 65, Magarula),
+        (52, 100, Diarahan),
+        (55, 100, Amrita),
+        (57, 80, Garudyne),
+        (60, 100, Wind_Break),
+        (65, 100, Magarudyne),
+        (68, 100, Samarecarm),
+        (74, 100, Mediarahan),
+        (80, 100, Panta_Rhei)
+]
+
+junpei = [
+        (1, 100, Spring_of_Life),
+        (1, 50, Re_Patra),
+        (1, 34, Rakukaja),
+        (1, 40, Double_Fangs),
+        (1, 55, Kill_Rush),
+        (1, 64, Agilao),
+        (1, 45, Counter),
+        (34, 100, Marakukaja),
+        (40, 75, Torrent_Shot),
+        (45, 58, Counterstrike),
+        (50, 65, Blade_of_Fury),
+        (55, 80, Gigantic_Fist),
+        (58, 100, High_Counter),
+        (60, 100, Power_Charge),
+        (64, 100, Agidyne),
+        (65, 100, Brave_Blade),
+        (75, 100, Vorpal_Blade_Answer),
+        (80, 100, Phys_Skill_Up)
+]
+
+akihiko = [
+        (1, 50, Sonic_Punch),
+        (1, 29, Zio),
+        (1, 38, Dia),
+        (1, 47, Tarunda),
+        (1, 41, Mazio),
+        (1, 57, Rakunda),
+        (29, 53, Zionga),
+        (33, 66, Sukunda),
+        (37, 76, Elec_Boost),
+        (38, 65, Diarama),
+        (41, 74, Mazionga),
+        (47, 100, Matarunda),
+        (50, 100, Fist_Master),
+        (53, 80, Ziodyne),
+        (57, 100, Marakunda),
+        (65, 100, Diarahan),
+        (66, 100, Debilitate),
+        (74, 100, Maziodyne),
+        (76, 100, Elec_Amp),
+        (80, 100, Thunder_Reign)
+]
+
+mitsuru = [
+        (1, 42, Mabufu),
+        (1, 50, Marin_Karin),
+        (1, 55, Bufula),
+        (1, 58, Diarama),
+        (1, 100, Spirit_Drain),
+        (1, 100, Tentarafoo),
+        (42, 71, Mabufula),
+        (45, 76, Ice_Boost),
+        (50, 100, Mind_Charge),
+        (55, 80, Bufudyne),
+        (58, 100, Diarahan),
+        (61, 100, Ice_Break),
+        (71, 100, Mabufudyne),
+        (76, 100, Ice_Amp),
+        (80, 100, Niflheim)
+]
+
+koromaru = [
+        (1, 45, Agilao),
+        (1, 33, Mudo),
+        (1, 50, Sukukaja),
+        (1, 100, Apt_Pupil),
+        (33, 71, Mudoon),
+        (34, 67, Maragion),
+        (39, 77, Fire_Boost),
+        (40, 75, Mighty_Swing),
+        (42, 49, Mamudo),
+        (45, 80, Agidyne),
+        (49, 100, Mamudoon),
+        (50, 100, Masukukaja),
+        (52, 100, High_Counter),
+        (60, 100, Mudo_Boost),
+        (67, 100, Maragidyne),
+        (71, 100, Desecrate),
+        (75, 100, Heavens_Blade),
+        (77, 100, Fire_Amp),
+        (80, 100, Ragnarok)
+
+]
+
+ken = [
+        (1, 35, Hama),
+        (1, 46, Cruel_Attack),
+        (1, 55, Zionga),
+        (1, 42, Sukunda),
+        (29, 60, Diarama),
+        (32, 71, Recarm),
+        (35, 74, Hamaon),
+        (37, 76, Mediarama),
+        (42, 100, Masukunda),
+        (46, 65, Vile_Assault),
+        (54, 100, Spear_Master),
+        (55, 80, Ziodyne),
+        (60, 100, Diarahan),
+        (65, 100, Primal_Force),
+        (71, 100, Samarecarm),
+        (74, 100, Sanctify),
+        (76, 100, Mediarahan),
+        (80, 100, Thunder_Reign)
+]
